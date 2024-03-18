c03971b003fa31ea28953d967981a24de4e99eb6
acdepak <acdepak@gmail.com>
2024-03-19 00:20:40 +0545

    used variable foe url

--- specific-tab-closer.js
+++ specific-tab-closer.js
@@ -1,22 +1,26 @@
 let defaultCloseAfterMs = 3000;
+let urlMessage = "youtube.com";
 
 browser.runtime.onMessage.addListener((message) => {
   if (message.closeAfterMs) {
     defaultCloseAfterMs = message.closeAfterMs;
   }
-  //this works
+  if (message.url) {
+    urlMessage = message.url;
+  }
 });
 
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   // console.log(`Updated Tab ID: ${tabId}, URL: ${tab.url}, Status: ${changeInfo.status}`);
 
-  if (changeInfo.status === "complete" && tab.url && tab.url.includes("youtube.com")) {
+  if (changeInfo.status === "complete" && tab.url && tab.url.includes(urlMessage)) {
     // console.log('tab.url.includes("youtube.com")', tab.url.includes("youtube.com"));
 
     setTimeout(() => {
       browser.tabs.remove(tabId);
     }, defaultCloseAfterMs);
-  } else {
-    // console.log('tab.url.includes("youtube.com")', tab.url.includes("youtube.com"));
   }
+  // else {
+  //   console.log('tab.url.includes("youtube.com")', tab.url.includes("youtube.com"));
+  // }
 });
